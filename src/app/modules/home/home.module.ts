a5721f6bfd8c81db3f305c10c4ce79e5219a0a33
danaaguilar <danaaguilar2017@gmail.com>
2023-07-03 09:39:42 -0300

    pude hacer la ruta para mostrar el carrousel

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -14,6 +14,7 @@ import { CardsEstaticasComponent } from './components/cards-estaticas/cards-esta
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[HomeComponent]
 })
 export class HomeModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -145,7 +145,7 @@ import {OverlayModule} from '@angular/cdk/overlay';
     MatTreeModule,
     OverlayModule,
     PortalModule,
-    ScrollingModule,
+    ScrollingModule
    ]
 })
 export class SharedModule { }

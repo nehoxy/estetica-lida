c84aea3b887f1bc2d08e30d70cc8d1ebfdcca6ba
nehoxy <lionelhugo7@gmail.com>
2023-07-04 23:23:03 -0300

    creacion de cada modulo con su routing y declaracion de componentes

--- src/app/modules/productos/productos.module.ts
+++ src/app/modules/productos/productos.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ProductosRoutingModule } from './productos-routing.module';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { CardProductoComponent } from './components/card-producto/card-producto.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
-
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { PaginationComponent } from './components/pagination/pagination.componen
     PaginationComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductosRoutingModule
   ]
 })
 export class ProductosModule { }

11c5833610c40aaa96354889097af923aea334d9
nehoxy <lionelhugo7@gmail.com>
2023-07-04 23:31:21 -0300

    auth lazyloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { zip } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>import('./modules/auth/auth.module').then(m => m.AuthModule)
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

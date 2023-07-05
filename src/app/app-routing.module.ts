d137288b6fdb426750e8caec89551c1cb45dab6f
nehoxy <lionelhugo7@gmail.com>
2023-07-04 23:49:03 -0300

    lazyloading de auth, home y productos terminado con rutas secundarias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,14 @@ const routes: Routes = [
     path:'auth',
     loadChildren:()=>import('./modules/auth/auth.module').then(m => m.AuthModule)
 
+  },
+  {
+    path:'home',
+    loadChildren:()=>import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path:'productos',
+    loadChildren:()=>import('./modules/productos/productos.module').then(m => m.ProductosModule)
   }
 ];
 

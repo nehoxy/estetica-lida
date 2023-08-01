c366b76fdec363be21fb2cea62dfa0ef52bb798c
nehoxy <lionelhugo7@gmail.com>
2023-08-01 18:26:38 -0300

    routing funcionando falta el componente

--- src/app/modules/administrador/administrador-routing.module.ts
+++ src/app/modules/administrador/administrador-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdministradorComponent } from './pages/administrador/administrador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:AdministradorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/administrador/administrador.module.ts
+++ src/app/modules/administrador/administrador.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdministradorRoutingModule } from './administrador-routing.module';
+import { AdministradorComponent } from './pages/administrador/administrador.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+
+  
+    AdministradorComponent
+  ],
   imports: [
     CommonModule,
     AdministradorRoutingModule

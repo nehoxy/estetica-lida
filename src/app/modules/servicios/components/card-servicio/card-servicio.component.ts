a2dce748e78bd43d58939bdd0378655ea50a4853
nehoxy <lionelhugo7@gmail.com>
2023-08-23 11:30:32 -0300

    intento de agregar el modal

--- src/app/modules/servicios/components/card-servicio/card-servicio.component.ts
+++ src/app/modules/servicios/components/card-servicio/card-servicio.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-card-servicio',
   templateUrl: './card-servicio.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class CardServicioComponent {
 
+ listaServicios:any = [
+  {
+    id:1, 
+    titulo:"Manicuria Rusa",
+    precio:2000,
+    imagen:"../../../../../assets/img-card-servicio/manicuria-rusa.jpg",
+    descripcion:"La manicura rusa se realiza con torno y consigue una limpieza profunda de la uña quitando totalmente la cutícula. Utilizando diferentes fresas o brocas, se retira la cutícula suavemente y se aplica el esmalte lo más cerca posible de ésta."
+  },
+  {
+    id:2, 
+    titulo:"Esmaltado Semi",
+    precio:3000,
+    imagen:"../../../../../assets/img-card-servicio/esmaltado-semi-permanente.jpg",
+    descripcion:"La manicura rusa se realiza con torno y consigue una limpieza profunda de la uña quitando totalmente la cutícula. Utilizando diferentes fresas o brocas, se retira la cutícula suavemente y se aplica el esmalte lo más cerca posible de ésta."
+  }
+ ]
 }

d142d02745744cf276bce9bb95d6f53bc249bd51
ittkou <martufuentes1@gmail.com>
2023-08-28 10:43:18 -0300

    agregue las columnas metodoenvio, envio, costoenvio, y montoTotal

--- src/app/modules/administrador/components/tabla-ventas-administrador/tabla-ventas-administrador.component.ts
+++ src/app/modules/administrador/components/tabla-ventas-administrador/tabla-ventas-administrador.component.ts
@@ -11,7 +11,7 @@ import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 })
 
 export class TablaVentasAdministradorComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position','cliente', 'pago', 'fecha', 'metodopago'];
+  displayedColumns: string[] = ['position','cliente', 'pago', 'fecha', 'metodopago','metodoenvio','envio','costoenvio','montoTotal'];
   dataSource = new MatTableDataSource<TablaVentas>(CLIENT_DATA);
 
   @ViewChild(MatPaginator) paginator:any = MatPaginator;
@@ -31,28 +31,32 @@ export class TablaVentasAdministradorComponent implements AfterViewInit {
   pago:string;
   fecha: string;
   metodopago: string;
+  metodoenvio:string;
+  envio:string;
+  costoenvio:string;
+  montoTotal:string;
   }
 
   const CLIENT_DATA: TablaVentas[] = [
-  {position: 1, cliente: 'Hydrogen', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 2, cliente: 'Helium', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo'},
-  {position: 3, cliente: 'Lithium', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo'},
-  {position: 4, cliente: 'Beryllium', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo'},
-  {position: 5, cliente: 'Boron', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo' },
-  {position: 6, cliente: 'Carbon', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo'}, 
-  {position: 7, cliente: 'Nitrogen', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 8, cliente: 'Oxygen', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo'},
-  {position: 9, cliente: 'Fluorine', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo'},
-  {position: 10, cliente: 'Neon', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 11, cliente: 'Sodium', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 12, cliente: 'Magnesium', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo'},
-  {position: 13, cliente: 'Aluminum', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 14, cliente: 'Silicon', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 15, cliente: 'Phosphorus', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 16, cliente: 'Sulfur', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 17, cliente: 'Chlorine', pago: 'realizado' , fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 18, cliente: 'Argon', pago: 'realizado' , fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 19, cliente: 'Potassium', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo'},
-  {position: 20, cliente: 'Calcium', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo' },
+  {position: 1, cliente: 'Hydrogen', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 2, cliente: 'Helium', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 3, cliente: 'Lithium', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 4, cliente: 'Beryllium', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 5, cliente: 'Boron', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000' },
+  {position: 6, cliente: 'Carbon', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'}, 
+  {position: 7, cliente: 'Nitrogen', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 8, cliente: 'Oxygen', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 9, cliente: 'Fluorine', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 10, cliente: 'Neon', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 11, cliente: 'Sodium', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 12, cliente: 'Magnesium', pago: 'realizado', fecha: '1/1/1' , metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 13, cliente: 'Aluminum', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 14, cliente: 'Silicon', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 15, cliente: 'Phosphorus', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 16, cliente: 'Sulfur', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 17, cliente: 'Chlorine', pago: 'realizado' , fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 18, cliente: 'Argon', pago: 'realizado' , fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 19, cliente: 'Potassium', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000'},
+  {position: 20, cliente: 'Calcium', pago: 'realizado', fecha: '1/1/1', metodopago:'efectivo', metodoenvio:'envio express', envio:'pendiente', costoenvio:'$1000', montoTotal:'$1000' },
   ];
 

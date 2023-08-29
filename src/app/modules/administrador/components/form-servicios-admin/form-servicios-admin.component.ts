6b28968035c7316ad462070c9cfe2c23c0b4f3c6
santydalinger <dalingersantiago8@gmail.com>
2023-08-29 10:47:27 -0300

    ya cree el form base, me falta agregar los inputs y darle estilo

---
+++ src/app/modules/administrador/components/form-servicios-admin/form-servicios-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-servicios-admin',
+  templateUrl: './form-servicios-admin.component.html',
+  styleUrls: ['./form-servicios-admin.component.css']
+})
+export class FormServiciosAdminComponent {
+
+}

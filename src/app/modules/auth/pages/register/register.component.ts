98f38f6239a41ca5d0018ccd405b88cdee336ec9
nehoxy <lionelhugo7@gmail.com>
2023-08-29 11:01:37 -0300

    creacion de servicio firestore

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  hide = true;
+  usuarios:Usuario = {
+    uid:'',
+    nombre:'',
+    apellido: '',
+    email:'',
+    contrasena:''
+  }
+  uid = ''
 }

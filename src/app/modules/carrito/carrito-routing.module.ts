c7790798e118c723d959255e3376fa99322478ab
danaaguilar <danaaguilar2017@gmail.com>
2023-08-29 10:43:29 -0300

    agregue el routing en carrito-routing y los selectores en el carrito.component.html

--- src/app/modules/carrito/carrito-routing.module.ts
+++ src/app/modules/carrito/carrito-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComponent } from './pages/carrito/carrito.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:CarritoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

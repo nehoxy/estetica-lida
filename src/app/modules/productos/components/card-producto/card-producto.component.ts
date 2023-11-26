ed4e3bb3b2bcd4589085d97fedc4bde3837dad7c
nehoxy <lionelhugo7@gmail.com>
2023-11-26 18:39:14 -0300

    todo funcionando y optimizado

--- src/app/modules/productos/components/card-producto/card-producto.component.ts
+++ src/app/modules/productos/components/card-producto/card-producto.component.ts
@@ -12,10 +12,6 @@ export class CardProductoComponent {
 
   listaProductos : Producto[] = [];
   
-  cremas:Producto[] = []
-  maquillajes:Producto[] = []
-  mascarillas:Producto[] = []
-  jabones:Producto[] = []
   busqueda:string = ''
   filtro:string = ''
 
@@ -30,57 +26,21 @@ export class CardProductoComponent {
   ngOnInit() :void {
    this.mostrarProducto()
 
-   if(this.filtro=='cremas'){
-    this.mostrarCremas()
-   }else{
-   }
   }
 
   mostrarProducto(){
     this.servicioProductosCrud.obtenerProducto().subscribe(producto => {
       this.listaProductos = producto;
-      this.mostrarCremas()
-      this.mostrarMaquillajes()
-      this.mostrarMascarillas()
-      this.mostrarJabones()
+  
     })
   }
 
-  mostrarCremas(){
-    this.listaProductos.forEach(producto =>{
-      if(producto.categoria === "cremas"){
-        this.cremas.push(producto)
-      }
-    })
-    this.mostrarProducto()
-  }
-  mostrarMaquillajes(){
-    this.listaProductos.forEach(producto =>{
-      if(producto.categoria === "maquillajes"){
-        this.maquillajes.push(producto)
-      }
-    })
-  }
-  mostrarMascarillas(){
-    this.listaProductos.forEach(producto =>{
-      if(producto.categoria === "mascarillas"){
-        this.mascarillas.push(producto)
-      }
-    })
-  }
-  
-  mostrarJabones(){
-    this.listaProductos.forEach(producto =>{
-      if(producto.categoria === "jabones"){
-        this.jabones.push(producto)
-      }
-    })
-  }
+
 
   comprarProducto(){
+    alert('asdadas')
     Swal.fire({
       icon: 'warning',
-      iconColor: '#C8ECCB',
       confirmButtonColor: '#BB8588',
       title:'¡Lo sentimos!',
       text: 'Esta funcion aún no esta disponible.',

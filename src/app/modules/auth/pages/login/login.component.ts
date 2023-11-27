712f56e3ecee9c7539431d8a4bd36edaf10df459
danaaguilar <danaaguilar2017@gmail.com>
2023-11-27 08:38:41 -0300

    comente el ts del login

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -16,12 +16,16 @@ import Swal from 'sweetalert2';
 export class LoginComponent {
   hide = true;
   hidePass = true;
+  //verifica si el usuario esta logeado o no
   isLogged:boolean;
+
+  //crea un nuevo form: email y contraseña
   formLogin = this.fb.group({
     email: new FormControl ('',[Validators.required, Validators.email]),
     contrasena: new FormControl('',[Validators.required, Validators.minLength(8)])
   })
 
+  //llama a la interfaz
   usuarios: Usuario = {
     uid:'',
     nombre:'',
@@ -30,16 +34,20 @@ export class LoginComponent {
     rol:'',
     contrasena:'' }
 
+    //inyectar los servicios y clases
     constructor(public servicioAuth:AuthService, public firestore:FirestoreService, public router:Router, public fb:FormBuilder, public logged:IsLoggedInService){}
 
+  //funcion asincronica para iniciar sesion
   async iniciarSesion(){
+    //crea la credencial que contiene el email y contraseña 
     const credenciales = {
       email:this.usuarios.email,
       contrasena:this.usuarios.contrasena
     }
+    //respuesta: llama al servicio auth para iniciar sesion con las credenciales 
     const res = await this.servicioAuth.iniciarSesion(credenciales.email,credenciales.contrasena)
     .then( res =>{
-  
+      //alert de incio exitoso
       Swal.fire({
         icon: 'success',
         iconColor: '#C8ECCB',
@@ -47,10 +55,13 @@ export class LoginComponent {
         title: 'Iniciaste sesion',
         text: 'Bienvenido/a '+credenciales.email+'!',
       })
+      //una vez iniciado sesion te redirecciona al home
       this.router.navigate(['home'])
+      //ahora el estado de islogged es true
       this.logged.setLoggedStatus(true);
       console.log(this.isLogged)
     }).catch(error =>{
+      //alert de error
       Swal.fire({
         icon: 'error',
         confirmButtonColor: '#BB8588',
